2e97e697f432f8ac4d4d12deba2c4ed42dedcd3d
kounotakuya <takuya00421@gmail.com>
2026-07-19 18:51:13 +0900

    Update Terms of Service URL

--- lib/app/constants/app_urls.dart
+++ lib/app/constants/app_urls.dart
@@ -2,5 +2,5 @@ class AppUrls {
   const AppUrls._();
 
   static const termsOfService =
-      'https://app.notion.com/p/JLPT-3a10936640a78009bb5ae9a936ef32d6?source=copy_link';
+      'https://prairie-fortnight-f1a.notion.site/JLPT-3a10936640a78009bb5ae9a936ef32d6';
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,8 @@ import firebase_auth
 import firebase_core
 import firebase_crashlytics
 import firebase_storage
+import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
